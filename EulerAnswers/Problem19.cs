05133ba1c73123c5127f535a90432dd60de7a115
Brooke Philpott <brphilpott@yahoo.com>
2020-07-28 21:25:21 -0400

    Converted project to .net core. Solutions to 19 and 20.

---
+++ EulerAnswers/Problem19.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerAnswers
+{
+    public class Problem19 : IAnswer
+    {
+        public string Answer()
+        {
+            DateTime startDate = new DateTime(1901, 1, 1);
+            DateTime endDate = new DateTime(2000, 12, 31);
+            int result = 0;
+            for (DateTime currentDate = startDate; currentDate < endDate; currentDate = currentDate.AddDays(1))
+            {
+                if (currentDate.Day == 1 && currentDate.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    result++;
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ EulerAnswers/Problem20.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerAnswers
+{
+    public class Problem20 : IAnswer
+    {
+        public string Answer()
+        {
+            BigInteger result = 1;
+            for (var i = 1; i <= 100; i++)
+            {
+                result *= i;
+            }
+
+            int summedCharacters = 0;
+            var resultCharacters = result.ToString().AsSpan();
+            for (int i=0;i<resultCharacters.Length;i++)
+            {
+                var value = int.Parse(resultCharacters.Slice(i, 1));
+                summedCharacters += value;
+            }
+
+            return summedCharacters.ToString();
+        }
+    }
+}
