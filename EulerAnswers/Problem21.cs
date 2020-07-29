fb522691f35698a2d5e3df8a1056c3266acedfc6
Brooke Philpott <brphilpott@yahoo.com>
2020-07-28 22:18:18 -0400

    Answers to 21 and 22

---
+++ EulerAnswers/Problem21.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerAnswers
+{
+    public class Problem21 : IAnswer
+    {
+        public string Answer()
+        {
+            var max = 10_000;
+            int[] amicableNumbers = new int[max];
+            HashSet<int> matchingNumbers = new HashSet<int>();
+            for (int baseValue = 1; baseValue < max; baseValue++)
+            {
+                for (int multiple = 2; multiple < max; multiple++) // no need to go above 500 because we can't really divide past this. We could probably even use a lower number, like max/4;
+                {
+                    var product = baseValue * multiple;
+                    if (product > baseValue && product < max)
+                    {
+                        amicableNumbers[product] += baseValue;
+                    }
+                    else
+                    {
+                        break; // short circuit here
+                    }
+                }
+
+                var thisNumber = amicableNumbers[baseValue];
+                if (thisNumber < 1)
+                {
+                    continue;
+                }
+                if (thisNumber > max)
+                {
+                    continue;
+                }
+                var otherNumber = amicableNumbers[thisNumber];
+                if (otherNumber > max)
+                {
+                    continue;
+                }
+                if (otherNumber != thisNumber &&
+                    amicableNumbers[otherNumber] == thisNumber)
+                {
+                    matchingNumbers.Add(thisNumber);
+                    matchingNumbers.Add(otherNumber);
+                }
+            }
+            var total = matchingNumbers.Sum();
+            return total.ToString();
+        }
+    }
+}
---
+++ EulerAnswers/Problem22.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerAnswers
+{
+    public class Problem22 : IAnswer
+    {
+        public string Answer()
+        {
+            var names = EulerAnswers.Properties.Resources.p022_names;
+            var namesList = names.Split(',').ToList();
+            namesList.Sort();
+            long totalForEntireFile = 0;
+            for (int i = 0; i < namesList.Count; i++)
+            {
+                var nameOffset = i + 1;
+                if (nameOffset == 938)
+                {
+                    System.Diagnostics.Debugger.Break();
+                }
+                var name = namesList[i].AsSpan();
+                var aValue = (byte)'A';
+                int nameValue = 0;
+                for (int charOffset = 1; charOffset < name.Length - 1; charOffset++)
+                {
+                    var distanceFromA = ((byte)name[charOffset] - aValue) + 1;
+                    nameValue += distanceFromA;
+                }
+                var nameTotal = nameOffset * nameValue;
+                totalForEntireFile += nameTotal;
+            }
+
+            return totalForEntireFile.ToString();
+        }
+    }
+}
